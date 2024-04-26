88cbf15cac3e5e5804aaed9449446310a9bd3da7
leo-lifecode <167279109+leo-lifecode@users.noreply.github.com>
2024-04-26 22:21:18 +0700

    feat: weather temp and humadity (#1)
    add weather temp and the other.
    and make it design 89%.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,44 @@
 import "./App.css";
 import Navbar from "./components/navbar/navbar";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import ImageCloudCelcius from "./components/ImageCloudCelcius/imageCloudCelcius";
+import CardToday from "./components/boxtoday/CardToday";
+import BoxNextForecast from "./components/boxNextForecast/BoxNextForecast";
 
 function App() {
-  const [dataCity, setDataCity] = useState([]);
+  const [dataCity, setDataCity] = useState(null);
+  const [fetchDataCity, setFetchDataCity] = useState(null);
 
   function onSearchChange(searchData) {
-    console.log(searchData);
+    setDataCity(searchData);
   }
+
+  useEffect(() => {
+    async function cloud() {
+      let lat = "";
+      let lon = "";
+      dataCity !== null ? ([lat, lon] = dataCity.value.split(" ")) : null;
+      const api_key = "d67e454852842837519116e10a7c2874";
+      const apiweather = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${
+          lat === "" ? "3.65" : lat
+        }&lon=${lon === "" ? "98.6667" : lon}&appid=${api_key}&units=metric`
+      );
+      const response = await apiweather.json();
+      console.log("ini response", response);
+      setFetchDataCity(response);
+    }
+    cloud();
+  }, [dataCity]);
 
   return (
     <div className="box">
       <Navbar onSearchChange={onSearchChange} />
+      <div className="homepage">
+        <ImageCloudCelcius fetchDataCity={fetchDataCity} />
+        <CardToday />
+        <BoxNextForecast />
+      </div>
     </div>
   );
 }
